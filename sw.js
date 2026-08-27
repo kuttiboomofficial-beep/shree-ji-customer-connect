3bbb379fdc51c5cc601c2f718bd18af0f1b31d23
kuttiboomofficial-beep <kuttiboomofficial@gmail.com>
2026-08-28 00:44:32 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE='sj-connect-v1';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg']))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{let copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html')))));
